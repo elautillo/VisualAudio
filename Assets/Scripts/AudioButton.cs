d62ae2f09ad7ac64cb0f54e7d374cfe174926c94
Miguel Alonso <mab603fake@gmail.com>
2019-04-09 19:25:02 +0200

    Initial commit

---
+++ Assets/Scripts/AudioButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioButton : MonoBehaviour
+{
+    AudioSource my_AudioSource;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        my_AudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayAudio()
+    {
+        my_AudioSource.Play();
+    }
+}
